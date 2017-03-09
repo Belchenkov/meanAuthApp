d5b25bbb794106535d107810fc24addded67a4a2
Aleksey Belchenkov <u608110@gmail.com>
2017-03-09 11:56:23 +0300

    Added Auth Service and User Registration

--- routes/users.js
+++ routes/users.js
@@ -35,21 +35,21 @@ router.post('/authenticate', (req, res, next) => {
     const password = req.body.password;
 
     User.getUserByUsername(username, (err, user) => {
-        if(err) throw err;
-        if(!user){
-            return res.json({success: false, msg: 'User not found'});
+        if (err) throw err;
+        if (!user) {
+            return res.json({ success: false, msg: 'User not found' });
         }
 
         User.comparePassword(password, user.password, (err, isMatch) => {
-            if(err) throw err;
-            if(isMatch){
+            if (err) throw err;
+            if (isMatch) {
                 const token = jwt.sign(user, config.secret, {
                     expiresIn: 604800 // 1 week
                 });
 
                 res.json({
                     success: true,
-                    token: 'JWT '+token,
+                    token: 'JWT ' + token,
                     user: {
                         id: user._id,
                         name: user.name,
@@ -58,15 +58,15 @@ router.post('/authenticate', (req, res, next) => {
                     }
                 });
             } else {
-                return res.json({success: false, msg: 'Wrong password'});
+                return res.json({ success: false, msg: 'Wrong password' });
             }
         });
     });
 });
 
 // Profile
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
-    res.json({user: req.user});
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.json({ user: req.user });
 });
 
 //ValidateS
